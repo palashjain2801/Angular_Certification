ad91091a35599e97f65f9eff39233d8506ab74e4
palashjain2801 <palashjain2801@gmail.com>
2021-05-04 15:05:49 -0500

    update

--- AngularProject/src/app/app-routing.module.ts
+++ AngularProject/src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { HomeComponent } from './MainScreen/home/home.component';
+import { OrderDetailsComponent } from './Order/order-details/order-details.component';
+import { ProfileComponent } from './Profile/profile/profile.component';
+import { UserSettingComponent } from './Setting/user-setting/user-setting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"Login",
+    component: LoginComponent
+  },
+  {
+    path:"Home",
+    component:HomeComponent
+  },
+  {
+    path:"Order",
+    component:OrderDetailsComponent
+  },
+  {
+    path:"Profile",
+    component:ProfileComponent
+  },
+  {
+    path:"Setting",
+    component:UserSettingComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
