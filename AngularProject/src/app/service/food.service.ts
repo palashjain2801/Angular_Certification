91dbc035f383c0ba6e0830549bba9d832e6d9fb2
palashjain2801 <palashjain2801@gmail.com>
2021-05-06 12:54:02 -0500

    update

--- AngularProject/src/app/service/food.service.ts
+++ AngularProject/src/app/service/food.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Food } from '../MainScreen/list-food/food';
+import { ListFoodComponent } from '../MainScreen/list-food/list-food.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FoodService {
+  private FoodList : any;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+  baseUrl: string = 'http://localhost:3000/foods';
+
+  
+  getAllFood(){
+    return this.httpClient.get<Food[]>(this.baseUrl);
+    //return this.allProducts;
+  }
+
+  getFood(id: number){
+  
+    return this.httpClient.get<Food>(this.baseUrl+'/'+id);
+    
+  }
+  // Deal with Data Base -> CURD Operation 
+  // Parent Class to Others -> Make this class injectasble to other class-> then share information
+  getListFood(test:object){
+    // this.FoodList.push(test)
+    this.FoodList = test;
+    console.log("service",this.FoodList)
+  }
+  getCartItem(){
+    return this.FoodList
+  }
+
 }
